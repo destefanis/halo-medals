7be52a6f56fd4881f6221b839b6b25166a50b8b8
destefanis <drdestefanis@gmail.com>
2016-01-10 22:57:06 -0600

    Added styles and markup for preloader and home form elements

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@
   'use strict'
 
   $('.js-button').click(function() {
-    console.log("clicked");
-    fetchMaps();
+    $('.preloader__background').addClass('is--loading');
+    fetchMaps(); 
+    $('.preloader__message').addClass('has-message');
+    $('.preloader__message').text("Fetching Recent Matches");
   });
   // Fetch the list of maps available in the game. We need this data
   // to compare our match Map ID to it's corresponding map later on.
@@ -38,7 +40,6 @@
       dataType: "json",
 
       success: function(medal_data) {
-        console.log(medal_data);
         fetchMatches(map_data, medal_data);
       }
     })
@@ -48,7 +49,7 @@
   function fetchMatches(map_data, medal_data) {
 
     $.jsonp({
-      url: "https://www.haloapi.com/stats/h5/players/shesjustaglitch/matches?modes=warzone&start=0&count=3",
+      url: "https://www.haloapi.com/stats/h5/players/shesjustaglitch/matches?modes=arena,warzone&start=0&count=5",
       beforeSend: function(xhrObj) {
         // Request headers
         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","ba39bd7104bf4cdf8385f925f2e709a1");
@@ -57,6 +58,7 @@
       dataType: "json",
       success: function(matchHistory) {
         DetermineMatchType(matchHistory, map_data, medal_data);
+        $('.preloader__message').text("Recent Matches Found");
       }
     })
     .done(function(data) {
@@ -64,6 +66,7 @@
     })
     .fail(function() {
       console.log("Call not successful.");
+      $('.preloader__message').text("Error: An error occured and the attempt to fetch messages wasn't successful. Please try again.");
     });
   }
 
@@ -78,12 +81,13 @@
 
       // Games Modes are stored as an ints in the API.
       if (gameMode === 1) {
-        //console.log('arena');
+        gameMode = "arena";
       }
       else if (gameMode === 4) {
         gameMode = "warzone";
-        FetchMatchDetails(matchId, map_data, medal_data, gameMode);
       }
+
+      FetchMatchDetails(matchId, map_data, medal_data, gameMode);
     });
   }
 
@@ -115,52 +119,61 @@
             match.MatchImage = this.imageUrl;
             match.id = matchId;
 
-            console.log(match);
+            $.each(match.PlayerStats, function(i, player) {
+              if (this.Player.Gamertag === "Shesjustaglitch") {
+                match.player = this;
+              }
+            })
 
             recentMatches.push(match);
-            
-            var matchCardTemplate = _.template($("#match-card").html());
-            $("#recent-matches").append(matchCardTemplate({match: match}));
           }
         })
 
-        // Loop through the players in the match and find
-        // the stats for the player by their gamertag.
-        $.each(match.PlayerStats, function(i, player) {
-          if (this.Player.Gamertag === "Shesjustaglitch") {
-            console.log(this);
-            console.log(this.TotalSpartanKills);
-            recentMatches.player = this;
-            console.log(recentMatches.player);
+        $.each(recentMatches, function( i, val ) {
+          var matchCardTemplate = _.template($("#match-card").html());
+          $("#recent-matches").append(matchCardTemplate({match: val}));
+        })
 
-            // Loop through the medals the player earned in this match.
-            $.each(this.MedalAwards, function(i, medal) {
+        $('.preloader__background').removeClass('is--loading');
+        $('.preloader__message').text("");
+        $('.preloader__message').removeClass('has-message');
 
-              // Medals in the Medal Array have ID's that are strings,
-              // where in the post carnage results they're ints, so we need
-              // to do a type conversion.
-              var medalId = medal.MedalId;
-              var Medalnum = medalId.toString();
+        $("#recent-matches").delegate(".match-card", "click", function (){
+          var selectedId = $(this).attr("data-id");
+          
+          $.each(recentMatches, function(i, match) {
+            if (match.id === selectedId) {
               
-              // Match the medal from our post game results to the medal array,
-              // using the medal id.
-              var result = _.findWhere(medals, {id: Medalnum});
-              // Include the amount of times this medal was earned in the match.
-              result.count = medal.Count;
-              // Add all the medal objects to an array that we can loop through
-              // in our template.
-              medalsEarned.push(result);
-            })
-          }
-        })
+              // Loop through the medals the player earned in this match.
+              $.each(match.player.MedalAwards, function(i, medal) {
 
-        var tableTemplate = _.template($("#medal-list").html());
-        var medalList = "#" + match.id;
-        $(medalList).append(tableTemplate({medalsEarned: medalsEarned}));
+                // Medals in the Medal Array have ID's that are strings,
+                // where in the post carnage results they're ints, so we need
+                // to do a type conversion.
+                var medalId = medal.MedalId;
+                var Medalnum = medalId.toString();
+                
+                // Match the medal from our post game results to the medal array,
+                // using the medal id.
+                var result = _.findWhere(medals, {id: Medalnum});
+
+                // Include the amount of times this medal was earned in the match.
+                result.count = medal.Count;
+
+                // Add all the medal objects to an array that we can loop through
+                // in our template.
+                medalsEarned.push(result);
+              })
+            }
+          })
+          
+          // Append the medals to the match card.
+          var tableTemplate = _.template($("#medal-list").html());
+          var medalList = "#" + match.id;
+          $(medalList).append(tableTemplate({medalsEarned: medalsEarned}));
+        })
       }
     })
   }
-
-  //var matchCard = _.template("<h1>Some text: <%= foo %></h1>");
 
 })(jQuery);
